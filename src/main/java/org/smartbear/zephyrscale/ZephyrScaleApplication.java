f7c3e76fa8beb46406187c3d37f5e5d42f37b62f
jakubkochanski <jakub.kochanski@smartbear.com>
2024-10-19 15:18:48 +0200

    ZS/Initial commit
    # initialized repository

---
+++ src/main/java/org/smartbear/zephyrscale/ZephyrScaleApplication.java
@@ -0,0 +1,13 @@
+package org.smartbear.zephyrscale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZephyrScaleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZephyrScaleApplication.class, args);
+    }
+
+}
